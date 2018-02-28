5dca15bbbae0179b2db82040b34480ac98c497fb
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-02-28 10:17:56 +0300

    Correct the docs

--- library/Eternity/Timestamped/Potoki/Consume.hs
+++ library/Eternity/Timestamped/Potoki/Consume.hs
@@ -7,10 +7,10 @@ import Potoki.Consume
 import qualified Eternity.Potoki.Consume as A
 
 
-{-| Same as A.writeToFile, only specialized to timestamped values. -}
+{-| Same as 'A.writeToFile', only specialized to timestamped values. -}
 writeToFile :: Serialize event => FilePath -> Consume (Timestamped event) (Either IOException ())
 writeToFile = A.writeToFile
 
-{-| Same as A.writeToDir, only specialized to timestamped values. -}
+{-| Same as 'A.writeToDir', only specialized to timestamped values. -}
 writeToDir :: Serialize event => FilePath -> Consume (Timestamped event) (Either IOException ())
 writeToDir = A.writeToDir
--- library/Eternity/Timestamped/Potoki/Produce.hs
+++ library/Eternity/Timestamped/Potoki/Produce.hs
@@ -7,10 +7,10 @@ import Potoki.Produce
 import qualified Eternity.Potoki.Produce as A
 
 
-{-| Same as A.readFromFile, only specialized to timestamped values. -}
+{-| Same as 'A.readFromFile', only specialized to timestamped values. -}
 readFromFile :: Serialize event => FilePath -> Produce (Either IOException (Either Text (Timestamped event)))
 readFromFile = A.readFromFile
 
-{-| Same as A.readFromDir, only specialized to timestamped values. -}
+{-| Same as 'A.readFromDir', only specialized to timestamped values. -}
 readFromDir :: Serialize event => FilePath -> Produce (Either IOException (Either Text (Timestamped event)))
 readFromDir = A.readFromDir
