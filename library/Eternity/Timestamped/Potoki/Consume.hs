4127c10c6fae7c62a577574d24d7629470a66f6e
vlastachu <vlastachu@gmail.com>
2018-08-23 13:47:33 +0300

    add flush flag from eternity package

--- library/Eternity/Timestamped/Potoki/Consume.hs
+++ library/Eternity/Timestamped/Potoki/Consume.hs
@@ -1,16 +1,24 @@
-module Eternity.Timestamped.Potoki.Consume
-where
+module Eternity.Timestamped.Potoki.Consume where
 
-import Eternity.Timestamped.Prelude hiding (fold)
-import Timestamp
-import Potoki.Consume
-import qualified Eternity.Potoki.Consume as A
+import           Eternity.Timestamped.Prelude
+                                         hiding ( fold )
+import           Timestamp
+import           Potoki.Consume
+import qualified Eternity.Potoki.Consume       as A
 
 
 {-| Same as 'A.writeToFile', only specialized to timestamped values. -}
-writeToFile :: Serialize event => FilePath -> Consume (Timestamped event) (Either IOException ())
+writeToFile
+  :: Serialize event
+  => Bool  -- ^ on `True` stream will be flushed on each event, on `False` buffering will be enabled
+  -> FilePath
+  -> Consume (Timestamped event) (Either IOException ())
 writeToFile = A.writeToFile
 
 {-| Same as 'A.writeToDir', only specialized to timestamped values. -}
-writeToDir :: Serialize event => FilePath -> Consume (Timestamped event) (Either IOException ())
+writeToDir
+  :: Serialize event
+  => Bool -- ^ on `True` stream will be flushed on each event, on `False` buffering will be enabled
+  -> FilePath
+  -> Consume (Timestamped event) (Either IOException ())
 writeToDir = A.writeToDir
